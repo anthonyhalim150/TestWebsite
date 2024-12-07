a938336e5513086a3177c03bfebc6f80499389ba
anthonyhalim150 <anthonyhalim153@gmail.com>
2024-12-07 23:18:40 +0800

    Added firebase and sql modules, depending on what I will use

--- server.js
+++ server.js
@@ -1,35 +1,13 @@
-const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
-const app = express();
-const port = 3000;
-
-// Database connection
-const db = mysql.createConnection({
+const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',  // Your MySQL username
-    password: 'yourpassword', // Your MySQL password
-    database: 'shop'
+    user: 'root',
+    password: 'ath150338',
+    database: 'your_database'
 });
 
-db.connect(err => {
+connection.connect((err) => {
     if (err) throw err;
-    console.log('Connected to MySQL');
-});
-
-// Serve static files (HTML, CSS, JS)
-app.use(express.static('public'));
-
-// API to fetch items
-app.get('/api/items', (req, res) => {
-    const sql = 'SELECT * FROM items';
-    db.query(sql, (err, results) => {
-        if (err) throw err;
-        res.json(results);
-    });
-});
-
-// Start the server
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log('Connected to MySQL!');
 });
