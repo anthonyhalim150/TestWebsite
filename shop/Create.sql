532d286832fc458cabd07a073da059121746e78e
anthonyhalim150 <anthonyhalim153@gmail.com>
2024-12-09 19:00:49 +0800

    Added descript

--- shop/Create.sql
+++ shop/Create.sql
@@ -3,6 +3,7 @@ USE ecommerce;
 CREATE TABLE items (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
+    description TEXT,
     price DECIMAL(10, 2) NOT NULL,
     stock INT NOT NULL,
     image VARCHAR(255)
