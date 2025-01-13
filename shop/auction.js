07e7ff4362b2d11b9374df7dd048cb3ea05bee10
anthonyhalim150 <anthonyhalim153@gmail.com>
2025-01-13 12:33:35 +0800

    Update auction.js

--- shop/auction.js
+++ shop/auction.js
@@ -35,7 +35,7 @@ const fetchHighestBid = async (itemId) => {
   try {
     const response = await fetch(`${API_URL}/highest-bid?auction_item_id=${itemId}`);
     const data = await response.json();
-    console.log(data);
+    console.log(data.highestBid);
     return data.highestBid || 0;
   } catch (error) {
     console.error("Error fetching highest bid:", error);
